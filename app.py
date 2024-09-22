1173af4a7664db0865e7221300cfca850cb3865d
Sowmyagarg1003 <sowmgarg@gmail.com>
2024-09-22 17:03:55 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,18 +1,59 @@
-from flask import Flask, request, jsonify
+import base64
+
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
+def process_file(file_b64):
+    try:
+        file_data = base64.b64decode(file_b64)
+        file_size_kb = len(file_data) / 1024  
+
+     
+        file_mime_type = "image/png"
+        
+        return {
+            "file_valid": True,
+            "file_mime_type": file_mime_type,
+            "file_size_kb": round(file_size_kb, 2)
+        }
+    except Exception as e:
+        return {
+            "file_valid": False,
+            "file_mime_type": None,
+            "file_size_kb": 0
+        }
+
+# POST method
 @app.route('/bfhl', methods=['POST'])
 def process_data():
     try:
         data = request.json.get('data', [])
-        user_id = "ss3310"  # Replace with actual logic to generate user_id
+        file_b64 = request.json.get('file_b64', None)
+
+        user_id = "Sowmya_Garg_10032004" 
         email = "ss3310@srmist.edu.in"
         roll_number = "RA2111003030017"
 
         numbers = [item for item in data if item.isdigit()]
         alphabets = [item for item in data if item.isalpha()]
-        highest_alphabet = sorted(alphabets, key=lambda x: x.lower(), reverse=True)[:1]
+        lowercase_alphabets = [item for item in alphabets if item.islower()]
+        
+        print("Lowercase Alphabets:", lowercase_alphabets)
+        highest_alphabet = max(lowercase_alphabets) if lowercase_alphabets else "No lowercase alphabet found"
+
+
+
+        if file_b64:
+            file_info = process_file(file_b64)
+        else:
+            file_info = {
+                "file_valid": False,
+                "file_mime_type": None,
+                "file_size_kb": 0
+            }
 
         response = {
             "is_success": True,
@@ -21,8 +62,10 @@ def process_data():
             "roll_number": roll_number,
             "numbers": numbers,
             "alphabets": alphabets,
-            "highest_alphabet": highest_alphabet
+            "highest_lowercase_alphabet": highest_alphabet,
+            **file_info 
         }
+
         return jsonify(response), 200
 
     except Exception as e:
